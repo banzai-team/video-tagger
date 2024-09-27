3e66e4f3ce5d608d5eb2f0871117f5b48ae96ef2
Olga Chvyrova <Chvyrova.o@mail.ru>
2024-09-27 22:24:43 +0300

    Add table

--- frontend/src/pages/MainPage/components/VideoTable.tsx
+++ frontend/src/pages/MainPage/components/VideoTable.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {VideoDataItem} from "@/pages/MainPage/MainPage.tsx";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import {Routes} from "@/Router";
+import {Badge} from "@/components/ui/badge";
 
 type VideoTableProps = {
     data: VideoDataItem[];
@@ -7,9 +10,40 @@ type VideoTableProps = {
 
 const VideoTable: React.FC<VideoTableProps> = ({data}) => {
     return (
-        <div>
-            {data.map((item, key) => <div key={`video-${key}`}>video - {item.name}</div>)}
-        </div>
+        <Table>
+            <TableHeader>
+                <TableHead>Название</TableHead>
+                <TableHead>Ссылка</TableHead>
+                <TableHead>Теги</TableHead>
+            </TableHeader>
+            <TableBody>
+                {data.map((item) => (
+                    <TableRow key={`video-${item.id}`}>
+                        <TableCell className="max-w-48 w-max">
+                            <a href={`${Routes.Video}/${item.id}`} className="font-medium">
+                                {item.name}
+                            </a>
+                        </TableCell>
+                        <TableCell className="max-w-40 overflow-hidden text-ellipsis whitespace-nowrap">
+                            <a
+                                href={item.source}
+                                target="_blank"
+                                className="font-medium"
+                            >
+                                {item.source}
+                            </a>
+                        </TableCell>
+                        <TableCell className="flex gap-2">
+                            {item.tags.map(item => (
+                                <Badge className="text-xs whitespace-nowrap" variant='default'>
+                                    {item}
+                                </Badge>)
+                            )}
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
     );
 };
 
