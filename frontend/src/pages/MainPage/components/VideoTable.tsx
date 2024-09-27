2b3de6243ff5eac772c19305399d72b86cb0d02c
chOlya <Chvyrova.o@mail.ru>
2024-09-28 00:37:09 +0300

    Add row click

--- frontend/src/pages/MainPage/components/VideoTable.tsx
+++ frontend/src/pages/MainPage/components/VideoTable.tsx
@@ -1,59 +1,66 @@
-import {VideoDataItem} from "@/pages/MainPage/MainPage.tsx";
-import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
-import {Routes} from "@/Router";
-import {Badge} from "@/components/ui/badge";
+import { VideoDataItem } from "@/pages/MainPage/MainPage.tsx";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Routes } from "@/Router";
+import { Badge } from "@/components/ui/badge";
+import { useNavigate } from "react-router-dom";
 
 type VideoTableProps = {
-    data: VideoDataItem[];
+  data: VideoDataItem[];
 };
 
-const VideoTable: React.FC<VideoTableProps> = ({data}) => {
-    const croppedTags = (tags) => {
-        const items = tags.length > 3 ? tags.slice(0, 3) : tags
-        const ellipsis = tags.length > 3 ? '...' : ''
+const VideoTable: React.FC<VideoTableProps> = ({ data }) => {
+  const navigate = useNavigate();
+  const onRowClick = (id: string) => {
+    navigate(`${Routes.Video}/${id}`);
+  };
 
-        return <>
-            {items.map(item => (
-                <Badge className="text-xs whitespace-nowrap" variant='default'>
-                    {item}
-                </Badge>)
-            )}
-            {ellipsis}
-        </>
-    }
+  const croppedTags = (tags: string[]) => {
+    const items = tags.length > 3 ? tags.slice(0, 3) : tags
+    const ellipsis = tags.length > 3 ? '...' : ''
 
-    return (
-        <Table>
-            <TableHeader>
-                <TableHead>Название</TableHead>
-                <TableHead>Ссылка</TableHead>
-                <TableHead>Теги</TableHead>
-            </TableHeader>
-            <TableBody>
-                {data.map((item) => (
-                    <TableRow key={`video-${item.id}`}>
-                        <TableCell className="max-w-48 w-max overflow-hidden text-ellipsis whitespace-nowrap">
-                            <a href={`${Routes.Video}/${item.id}`} className="font-medium">
-                                {item.name}
-                            </a>
-                        </TableCell>
-                        <TableCell className="max-w-40 overflow-hidden text-ellipsis whitespace-nowrap">
-                            <a
-                                href={item.source}
-                                target="_blank"
-                                className="font-medium"
-                            >
-                                {item.source}
-                            </a>
-                        </TableCell>
-                        <TableCell className="flex gap-2">
-                            {croppedTags(item.tags)}
-                        </TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    );
+    return <>
+      {items.map(item => (
+        <Badge className="text-xs whitespace-nowrap" variant='secondary'>
+          {item}
+        </Badge>)
+      )}
+      {ellipsis}
+    </>
+  }
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableHead>Название</TableHead>
+        <TableHead>Ссылка</TableHead>
+        <TableHead>Теги</TableHead>
+      </TableHeader>
+      <TableBody>
+        {data.map((item) => (
+          <TableRow key={`video-${item.id}`} onClick={() => onRowClick(item.id)}>
+            <TableCell className="max-w-48 w-max overflow-hidden text-ellipsis whitespace-nowrap font-medium">
+              {item.name}
+            </TableCell>
+            <TableCell className="max-w-40 overflow-hidden text-ellipsis whitespace-nowrap">
+              <a
+                href={item.source}
+                target="_blank"
+                className="font-medium"
+                onClick={(event) => {
+                  event.stopPropagation();
+                }}
+              >
+                {item.source}
+              </a>
+            </TableCell>
+            <TableCell className="flex gap-2">
+              {croppedTags(item.tags)}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default VideoTable;
